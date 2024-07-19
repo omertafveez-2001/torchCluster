9571d376cd4d149386fdf28317bba4575fd4eed7
Omer Tafveez <omertafveez@gmail.com>
2024-07-19 18:57:57 +0500

    Refactored

--- src/torch_kmeans/clustering/soft_kmeans.py
+++ src/torch_kmeans/clustering/soft_kmeans.py
@@ -46,7 +46,7 @@ class SoftKMeans(KMeans):
 
     def __init__(
         self,
-        init_method: str = "k-means++",
+        init_method: str = "rnd",
         num_init: int = 1,
         max_iter: int = 80,
         distance: BaseDistance = CosineSimilarity,
